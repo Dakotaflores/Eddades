cdd22723eb9f24c3ad293bf166ba9fbec84fa6b7
Dakotaflores <Dakotaflores981@gmail.com>
2024-04-16 16:29:43 -0400

    Cambio en el codigo fuente

--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la edad:");
+        int edad = Convert.ToInt32(Console.ReadLine());
+
+        if (edad <= 0)
+        {
+            Console.WriteLine("La edad debe ser mayor a 0.");
+        }
+        else
+        {
+            if (edad <= 3)
+            {
+                Console.WriteLine("La persona es un recién nacido.");
+            }
+            else if (edad <= 9)
+            {
+                Console.WriteLine("La persona es un puberto.");
+            }
+            else if (edad <= 19)
+            {
+                Console.WriteLine("La persona es un adolescente.");
+            }
+            else if (edad <= 29)
+            {
+                Console.WriteLine("La persona es un joven.");
+            }
+            else if (edad <= 49)
+            {
+                Console.WriteLine("La persona es un adulto.");
+            }
+            else if (edad <= 59)
+            {
+                Console.WriteLine("La persona es un adulto mayor.");
+            }
+            else
+            {
+                Console.WriteLine("La persona es un anciano.");
+            }
+        }
+    }
+}
